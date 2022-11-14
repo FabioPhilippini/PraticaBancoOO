be4386de0f4278462907e85af2580b1c61fb7cd0
FabioPhilippini <fabioandre381@gmail.com>
2022-11-13 23:34:43 -0300

    Menu adicionado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,55 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import menu.MenuImpl;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		MenuImpl menu = new MenuImpl();
+		int opcao = menu.obterOpcao();
 
+		while (opcao != 9) {
+			switch (opcao) {
+			case 1:
+				menu.listarContas();
+				break;
+			case 2:
+				menu.inserirConta();
+				break;
+			case 3:
+				menu.sacar();
+				break;
+			case 4:
+				menu.depositar();
+				break;
+			case 5:
+				menu.transferir();
+				break;
+			case 6:
+				menu.exibirDados();
+				break;
+			case 7:
+				menu.dadosCliente();
+				break;
+			case 8:
+				menu.removerConta();
+				break;
+			default:
+				System.err.println("Opção Inválida");
+				System.out.println();
+			}
+			opcao = menu.obterOpcao();
+		}
+		System.out.println("Obrigado por utilizar nossos serviços");
+		System.out.println();
+
+		sc.close();
 	}
 
 }
